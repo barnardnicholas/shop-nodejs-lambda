10b278f44e3268a62507c348ffd62a4819958ed6
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2022-10-12 10:33:15 +0100

    Add node & typescript templates

--- product-service/handler.js
+++ product-service/handler.js
@@ -3,14 +3,10 @@
 module.exports.hello = async (event) => {
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
+    body: {
+        productName: 'Book',
+        price: 123,
       },
-      null,
-      2
-    ),
   };
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
